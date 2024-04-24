af85d32a10aee13e83443d5c68e7043281903171
email <my030@hdm-stuttgart.de>
2024-04-24 15:14:48 +0200

    Logo Fehler gelöst

--- frontend/src/components/loginbox.js
+++ frontend/src/components/loginbox.js
@@ -4,7 +4,7 @@ import { UserAuth } from "../context/AuthContext";
 import { Link } from "react-router-dom";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import Logo from "../images/logo.png";
+import Logo from "../images/logo-sf.png";
 
 const Loginbox = () => {
   const { user, logOut } = UserAuth();
