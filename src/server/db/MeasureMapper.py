c050afc72f4e1963b3d4a5c45cde04aada1181c2
Furkan Agcakoc <fa027@hdm-stuttgart.de>
2024-06-25 17:15:18 +0200

    Co-authored-by: Mehmet-Akif Yavuz <Mehmet-Akif-Yavuz@users.noreply.github.com> Co-authored-by: Baran Kocabey <Baran-Kocabey@users.noreply.github.com>

--- src/server/db/MeasureMapper.py
+++ src/server/db/MeasureMapper.py
@@ -77,8 +77,20 @@ class MeasureMapper(Mapper):
 
 
     def insert(self, measure):
-
         cursor = self._cnx.cursor()
+        
+        
+        check_command = "SELECT id FROM measure WHERE unit = %s AND fridge_id = %s"
+        check_data = (measure.get_unit(), measure.get_fridge_id())
+        cursor.execute(check_command, check_data)
+        result = cursor.fetchone()
+
+        if result is not None:
+            
+            cursor.close()
+            return measure
+
+        
         cursor.execute("SELECT MAX(id) AS maxid FROM measure")
         tuples = cursor.fetchall()
 
@@ -88,14 +100,16 @@ class MeasureMapper(Mapper):
             else:
                 measure.set_id(1)
 
-        command = "INSERT INTO measure (id, unit, fridge_id) VALUES (%s,%s,%s)"
-        data = (measure.get_id(), measure.get_unit(), measure.get_fridge_id())
-        cursor.execute(command, data)
+        
+        insert_command = "INSERT INTO measure (id, unit, fridge_id) VALUES (%s,%s,%s)"
+        insert_data = (measure.get_id(), measure.get_unit(), measure.get_fridge_id())
+        cursor.execute(insert_command, insert_data)
 
         self._cnx.commit()
         cursor.close()
 
         return measure
+
 
     def update(self, measure):
         cursor = self._cnx.cursor()
